7cdc2738c93791b9c013984fb7a431e6f8c034b1
taftkhris <chr13026@byui.edu>
2018-05-08 16:30:28 -0600

    adding select options to form attempt 4

--- web/teamActivity03.php
+++ web/teamActivity03.php
@@ -13,14 +13,15 @@ Major: <br>
 Comments <br>
 <textarea name="comments" rows="5" columns="40"></textarea> <br>
 Continents Visited:<br>
-<select name="continents" size="7" multiple>
-<option value="North America">North America</option>
-<option value="South America">South America</option>
-<option value="Europe">Europe</option>
-<option value="Asia">Asia</option>
-<option value="Australia">Australia</option>
-<option value="Africa">Africa</option>
-<option value="Antarctica">Antarctica</option>
+<select name="continents[]" size="7" multiple>
+    <option value="North America">North America</option>
+    <option value="South America">South America</option>
+    <option value="Europe">Europe</option>
+    <option value="Asia">Asia</option>
+    <option value="Australia">Australia</option>
+    <option value="Africa">Africa</option>
+    <option value="Antarctica">Antarctica</option>
+</select>
 <br>
 
 <input type="submit">
