e0194e058f9db79bb5e872090c18f83f9b2205e1
taftkhris <chr13026@byui.edu>
2018-05-08 16:23:21 -0600

    adding select options to form

--- web/form.php
+++ web/form.php
@@ -1,8 +1,12 @@
 <html>
     <body>
     User: <?php echo $_POST["name"]; ?><br>
-    Email Address is: <a href="mailto: <?php echo $_POST["email"];?>"</a><br>
+    Email Address is: <a href="mailto: <?php echo $_POST["email"];?>"><?php echo $_POST["email"];?></a><br>
     Major: <?php echo $_POST["major"]; ?><br>
-    Comments: <p><?php echo $_POST["major"]; ?></p><br>
+    Comments: <p><?php echo $_POST["comments"]; ?></p><br>
+    Visited Continents: <br>
+    <?php 
+    foreach ($_POST["continents"] as $visted)
+        echo("    $visted<br>")?>
     </body>
 </html>
--- web/teamActivity03.php
+++ web/teamActivity03.php
@@ -12,6 +12,17 @@ Major: <br>
 <input type="radio" name="major" value="compE">Computer Engineering<br>
 Comments <br>
 <textarea name="comments" rows="5" columns="40"></textarea> <br>
+Continents Visited:<br>
+<select name="continents" size="7" multiple>
+<option value="North America">North America</option>
+<option value="South America">South America</option>
+<option value="Europe">Europe</option>
+<option value="Asia">Asia</option>
+<option value="Australia">Australia</option>
+<option value="Africa">Africa</option>
+<option value="Antarctica">Antarctica</option>
+
+
 <input type="submit">
 </form>
 
