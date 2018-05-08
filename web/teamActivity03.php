8720464abb5a6db0d429cece12fd2945695f4948
taftkhris <chr13026@byui.edu>
2018-05-08 15:52:32 -0600

    implementing a form in team activity

---
+++ web/teamActivity03.php
@@ -0,0 +1,17 @@
+<html>
+<body>
+
+<form action="about_user.php" method="post">
+Name: <input type="text" name="name"><br>
+E-mail: <input type="text" name="email"><br>
+
+Major:
+<input type="radio" name="Computer Science" value="ComputerScience">Computer Science
+<input type="radio" name="Web D and D" value="webDesign">Web Design and Development
+<input type="radio" name="CIT" value="CIT">CIT
+<input type="radio" name="Comp E" value="compE">Computer Engineering
+<input type="submit">
+</form>
+
+</body>
+</html>
