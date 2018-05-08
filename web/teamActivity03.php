ee615fbef9114a80e9cd4d211216514ecbe57131
taftkhris <chr13026@byui.edu>
2018-05-08 15:56:45 -0600

    added text area to web page

--- web/teamActivity03.php
+++ web/teamActivity03.php
@@ -5,11 +5,13 @@
 Name: <input type="text" name="name"><br>
 E-mail: <input type="text" name="email"><br>
 
-Major:
-<input type="radio" name="Computer Science" value="ComputerScience">Computer Science
-<input type="radio" name="Web D and D" value="webDesign">Web Design and Development
-<input type="radio" name="CIT" value="CIT">CIT
-<input type="radio" name="Comp E" value="compE">Computer Engineering
+Major: <br>
+<input type="radio" name="Computer Science" value="ComputerScience">Computer Science <br>
+<input type="radio" name="Web D and D" value="webDesign">Web Design and Development <br>
+<input type="radio" name="CIT" value="CIT">CIT <br>
+<input type="radio" name="Comp E" value="compE">Computer Engineering<br>
+Comments <br>
+<textarea name="commets" rows="5" columns="40"></textarea>
 <input type="submit">
 </form>
 
