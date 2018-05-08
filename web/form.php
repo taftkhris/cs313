71f2dbbf2f1507031c084278723f6c97ed33230b
taftkhris <chr13026@byui.edu>
2018-05-08 16:13:48 -0600

    changed the action attempt 2

---
+++ web/form.php
@@ -0,0 +1,8 @@
+<html>
+    <body>
+    User: <?php echo $_POST["name"]; ?><br>
+    Email Address is: <a href="mailto: <?php echo $_POST["email"];?>"</a><br>
+    Major: <?php echo $_POST["major"]; ?><br>
+    Comments: <p><?php echo $_POST["major"]; ?></p><br>
+    </body>
+</html>
