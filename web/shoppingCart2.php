b9244b8ad9c15ad02ccffb0f7ee6a7e69aca603d
taftkhris <chr13026@byui.edu>
2018-05-12 12:06:14 -0600

    simple text to page using php

---
+++ web/shoppingCart2.php
@@ -0,0 +1,17 @@
+<?php 
+    echo 
+"<div class='row'> 
+    <div class='column' style='background-color:#aaa;'>
+    <h2>Column 1</h2>
+    <p>Some text..</p>
+  </div>
+  <div class='column' style='background-color:#bbb;'>
+    <h2>Column 2</h2>
+    <p>Some text..</p>
+  </div>
+  <div class='column' style='background-color:#ccc;'>
+    <h2>Column 3</h2>
+    <p>Some text..</p>
+  </div>
+</div>"
+?>
