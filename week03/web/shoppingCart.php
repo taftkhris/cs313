005c0a53e0ed623f9963c0771199c1f99795aac0
taftkhris <chr13026@byui.edu>
2018-05-12 11:42:41 -0600

    performing simple tests

--- week03/web/shoppingCart.php
+++ week03/web/shoppingCart.php
@@ -1,3 +1,14 @@
-<?php 
-    echo "<div class='row'> <h2>Hello World</h2></div>"
-?>
+<DOCTYPE <!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Page Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <script src="main.js"></script>
+</head>
+<body>
+    <h1>Hello World</h1>
+</body>
+</html>>
