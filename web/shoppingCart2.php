cf1b0a787743801e1ce6b049b43feb33ea49b81c
taftkhris <chr13026@byui.edu>
2018-05-12 13:02:38 -0600

    adjusting text under the pictures test 4

--- web/shoppingCart2.php
+++ web/shoppingCart2.php
@@ -6,19 +6,19 @@ echo "<div class='row'>
     <div class='column' style='background-color:#aaa;'>
     <img class='pictures' src='gtx.jpg' alt='redGtx'>
     <h2 class='carType'>67' GTX</h2>
-    <h3 class'price'>Price: $300</h3>
+    <h3 class='price'>Price: $300</h3>
     <p></p>
   </div>
   <div class='column' style='background-color:#bbb;'>
   <img class='pictures' src='nova.jpg' alt='redNova'>
     <h2 class='carType'>67' Nova</h2>
-    <h3 class'price'>Price: $500</h3>
+    <h3 class='price'>Price: $500</h3>
     <p></p>
   </div>
   <div class='column' style='background-color:#ccc;'>
   <img class='pictures' src='nomad.jpg' alt='chevyNomad'>
     <h2 class='carType'>56' Chevy</h2>
-    <h3 class'price'>Price: $400</h3>
+    <h3 class='price'>Price: $400</h3>
     <p></p>
   </div>
 </div>";
