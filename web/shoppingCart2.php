8e2d6fc0967548ed1433209faff5ffe9124edb1c
taftkhris <chr13026@byui.edu>
2018-05-12 12:35:45 -0600

    adjusting picture size and adding nova picture

--- web/shoppingCart2.php
+++ web/shoppingCart2.php
@@ -4,12 +4,13 @@ echo " <head>
 </head>"; 
 echo "<div class='row'> 
     <div class='column' style='background-color:#aaa;'>
-    <img id='gtx' src='gtx.jpg' alt='redGtx'>
+    <img class='pictures' src='gtx.jpg' alt='redGtx'>
     <h2>67' GTX</h2>
     <h3>Price: $300</h3>
     <p></p>
   </div>
   <div class='column' style='background-color:#bbb;'>
+  <img class='pictures' src='nova.jpg' alt='redNova'>
     <h2>67' Nova</h2>
     <h3>Price: $500</h3>
     <p>Some text..</p>
