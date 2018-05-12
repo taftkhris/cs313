563f5adbfeff68f8365071d615755d5a63cdd7da
taftkhris <chr13026@byui.edu>
2018-05-12 16:34:30 -0600

    attemping json string as value

--- web/shoppingCart2.php
+++ web/shoppingCart2.php
@@ -9,19 +9,19 @@ echo
     <img class='pictures' src='gtx.jpg' alt='redGtx'>
     <h2 class='carType'>67' GTX</h2>
     <h3 class='price'>Price: $300</h3>
-    <button type='submit' class='submitButton' name='gtx' value='300'>Add to Cart</button>
+    <button type='submit' class='submitButton' name='gtx' value='{\"type\":\"gtx\",\"price\":\"300\"}'>Add to Cart</button>
   </div>
   <div class='column' style='background-color:#bbb;'>
   <img class='pictures' src='nova.jpg' alt='redNova'>
     <h2 class='carType'>67' Nova</h2>
     <h3 class='price'>Price: $500</h3>
-    <button type='submit' class='submitButton' name='nova' value='500'>Add to Cart</button>
+    <button type='submit' class='submitButton' name='nova' value='Nova'>Add to Cart</button>
   </div>
   <div class='column' style='background-color:#ccc;'>
   <img class='pictures' src='nomad.jpg' alt='chevyNomad'>
     <h2 class='carType'>56' Chevy</h2>
     <h3 class='price'>Price: $400</h3>
-    <button type='button' class='submitButton' name='chevy' value='400'>Add to Cart</button>
+    <button type='submit' class='submitButton' name='chevy' value='Chevy'>Add to Cart</button>
   </div>
   <div>
     <a href='cartPage.php'>View Cart</a>
