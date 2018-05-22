c2e62ceb315f3ba306bceb6331f0935a4774c874
taftkhris <chr13026@byui.edu>
2018-05-22 16:10:47 -0600

    adding team activity file

---
+++ web/ta05.php
@@ -0,0 +1,39 @@
+<DOCTYPE <!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Page Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <script src="main.js"></script>
+</head>
+<body>
+    <h1>Scripture Resources</h1>
+    <?php
+    $dbUrl = getenv('DATABASE_URL');
+
+    $dbopts = parse_url($dbUrl);
+    
+    $dbHost = $dbopts["host"];
+    $dbPort = $dbopts["port"];
+    $dbUser = $dbopts["user"];
+    $dbPassword = $dbopts["pass"];
+    $dbName = ltrim($dbopts["path"],'/');
+    
+    $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+    
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
+    foreach ($db->query('SELECT book, chapter, verse, content FROM scriptures') as $row)
+    {
+    echo 'Book: ' . $row['book'];
+    echo ' Chapter: ' . $row['chapter'];
+    echo ' Verse: ' . $row['verse'];
+    echo ' Content: ' . $row['content'];
+    echo '<br/>';
+    }
+    ?>    
+</body>
+</html>
+
