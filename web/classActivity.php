3eee92863a7e8e92a990be713062bd6ac994f5d4
taftkhris <chr13026@byui.edu>
2018-05-31 15:35:41 -0600

    adding class activity files

---
+++ web/classActivity.php
@@ -0,0 +1,25 @@
+<?php 
+
+?>
+
+<!<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Page Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <script src="main.js"></script>
+</head>
+<body>
+    <h1>Courses</h1>
+
+    <ul> 
+        <li><p>Course 1<p></li>
+        <li><p>Course 2<p></li>
+        <li><p>Course 3<p></li>
+        <li><p>Course 4<p></li>
+    </ul>
+</body>
+</html>
