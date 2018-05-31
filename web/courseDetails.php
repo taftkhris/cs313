4fa74b4867931568aaf73af14eac00324ef9263b
taftkhris <chr13026@byui.edu>
2018-05-31 16:36:02 -0600

    adding forum to page

---
+++ web/courseDetails.php
@@ -0,0 +1,39 @@
+<?php 
+require("dbConnect.php");
+$courseId = htmlspecialchars($_GET["course_id"]);
+
+$db = get_db();
+
+$qeury = "SELECT name, number FROM course WHERE id";
+$statement = $db->prepare($query);
+
+$statement->bindValue(":id", $courseId, PDO::PARAM_INT);
+$statement->execute();
+
+$row = $statement->fetch();
+
+$number = $row['number'];
+$name = $row['name'];
+?>
+
+<!<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Page Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <script src="main.js"></script>
+</head>
+<body>
+    <?php 
+    echo "<h1>Showing notes for: $number - $name</h1>";
+    ?>
+
+    <form action"insertNote.php" method="POST">
+    <input type="hidden" name="course_id" value="<?php echo $courseId; ?>">
+    <input type="date" name="date"><br>
+    <textarea name="content" placeholder="content"> </textarea>
+</body>
+</html>
